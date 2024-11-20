1b2b06f20d331cac46569ce3768538bc2ef460a5
litvinov <litvinov@kraud.ru>
2024-11-20 20:18:28 +0300

    delete func added [lab-5]

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,21 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
     const galleryContainer = document.getElementById('gallery-container');
 
-    const mushrooms = JSON.parse(localStorage.getItem('mushrooms')) || [];
+    const loadMushrooms = () => JSON.parse(localStorage.getItem('mushrooms')) || [];
 
-    if (mushrooms.length === 0) {
-        galleryContainer.innerHTML = '<p>Нет добавленных грибов.</p>';
-    } else {
-        mushrooms.forEach((mushroom) => {
-            const mushroomCard = document.createElement('div');
-            mushroomCard.classList.add('mushroom-card');
-            mushroomCard.innerHTML = `
+    saveMushrooms = (mushrooms) => localStorage.setItem('mushrooms', JSON.stringify(mushrooms));
+
+    const update = () => {
+
+        const mushrooms = loadMushrooms();
+        galleryContainer.innerHTML = '';
+
+        if (mushrooms.length === 0) {
+            galleryContainer.innerHTML = '<p>Нет добавленных грибов.</p>';
+        } else {
+            mushrooms.forEach((mushroom, index) => {
+                const mushroomCard = document.createElement('div');
+                mushroomCard.classList.add('mushroom-card');
+                mushroomCard.innerHTML = `
                 <h3>${mushroom.name}</h3>
                 <img src="${mushroom.image}" alt="${mushroom.name}">
                 <p><strong>Тип:</strong> ${mushroom.type}</p>
                 <p><strong>Комментарий:</strong> ${mushroom.comment}</p>
+                <button class="delete-button" data-index=${index}>Удалить</button>
             `;
-            galleryContainer.appendChild(mushroomCard);
-        });
-    }
+                galleryContainer.appendChild(mushroomCard);
+            });
+        }
+    };
+    galleryContainer.addEventListener('click', (e) => {
+        if (e.target.classList.contains('delete-button')) {
+            const mushrooms = loadMushrooms();
+            const index = e.target.dataset.index;
+            mushrooms.splice(index, 1);
+            saveMushrooms(mushrooms);
+            update();
+        }
+    });
+    update();
 });
